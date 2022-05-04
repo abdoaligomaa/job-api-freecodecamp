585479ea3a75caf9454b8fedcf8d507b135b352b
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-05-04 16:30:46 +0200

    adding middlewares for not found end errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const connect=require('./db/connect')
-const User=require('./model/auth')
+const User=require('./models/user')
 const jobRouter=require('./router/job')
+const authRouter=require('./router/auth')
 const errorHandeler=require('./middleware/errorHandeler')
 const notfoundPage=require('./middleware/notfound')
 
@@ -11,15 +12,16 @@ const port=process.env.MONGOURL||3000
 const DbUrl='mongodb://127.0.0.1:27017/job_api'
 
 app.use(express.json())
-app.use('api/v1/auth',userRouter)
-app.use('api/v1/job',jobRouter)
-app.use(notfoundPage)
-app.use(errorHandeler)
+// app.use('api/v1/auth',userRouter)
+// app.use('api/v1/job',jobRouter)
 
 app.get('/',(req,res)=>{
-    res.send('Job Api')
+    throw Error("page not found")
+    // res.send('Job Api')
 })
 
+app.use(notfoundPage)
+app.use(errorHandeler)
 
 
 const start=async()=>{
