a0c37a66a57211d870618c481d56a4f2cefc039d
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-08-21 17:16:19 +0200

    adding prettier

--- controller/auth.js
+++ controller/auth.js
@@ -1,51 +1,51 @@
-const User=require('../models/user')
-const jwt=require('jsonwebtoken')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 // const { create } = require('../models/user')
 require('express-async-errors')
 // const {sendEmail}=require('../email/nodemailer')
-const CustomError=require('../error/customError')
+const CustomError = require('../error/customError')
 
-const signUP=async(req,res,next)=>{
+const signUP = async (req, res, next) => {
     // validate req.body
     // create new user
     // create Token
-    const {name,email,password}=req.body
-    if(!name||!email||!password){
-        throw  new CustomError('Please provide all name , email and password',404)
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        throw new CustomError(
+            'Please provide all name , email and password',
+            404
+        )
     }
-        const user = await User.create(req.body);
-        // sendEmail()
-        const token=user.generateToken()
-        res.json({
-            user,
-            token
-        })
+    const user = await User.create(req.body)
+    // sendEmail()
+    const token = user.generateToken()
+    res.json({
+        user,
+        token,
+    })
 }
-const logIN=async(req,res,next)=>{
+const logIN = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new CustomError('please provide both email and password', 404)
+    }
+    const user = await User.findOne({ email: email })
+    if (!user) {
+        throw new CustomError('please enter correct email', 404)
+    }
 
-        
-        const {email,password}=req.body
-        if(!email||!password){
-           throw new CustomError('please provide both email and password',404)
-        }
-        const user=await User.findOne({email:email})
-        if(!user){
-           throw new CustomError('please enter correct email',404)
-        }
-    
-        const isPassCorrect=await user.checkPassword(password)
-        if(!isPassCorrect){
-           throw new CustomError('please Enter Correct Password',404)
-        }
-        const token=user.generateToken()
-    
-        res.json({
-                user:user,
-                token
-            })
+    const isPassCorrect = await user.checkPassword(password)
+    if (!isPassCorrect) {
+        throw new CustomError('please Enter Correct Password', 404)
+    }
+    const token = user.generateToken()
 
+    res.json({
+        user: user,
+        token,
+    })
 }
-module.exports={
+module.exports = {
     signUP,
-    logIN
+    logIN,
 }
