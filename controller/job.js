6c239ea5d86c84f32de252d3caf51fb3ebbecda8
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-08-23 22:24:51 +0200

    get job by id

--- controller/job.js
+++ controller/job.js
@@ -6,10 +6,10 @@ require('express-async-errors')
 
 const getAllJobs = async (req, res, next) => {
     const userId = req.user.userId
-    
+
     const jobs = await Job.find({ createdBy: userId })
     if (!jobs) {
-        throw new customError('there are No job for you ',404)
+        throw new customError('there are No job for you ', 404)
     }
     res.json({ count: jobs.length, jobs })
 }
@@ -17,44 +17,49 @@ const getJob = async (req, res, next) => {
     // var jobId = mongoose.Types.ObjectId(req.params.id)
     const jobId = req.params.id
     try {
-        const job = await Job.findById(jobId)
+        const job = await Job.findOne({
+            _id: jobId,
+            createdBy: req.user.userId,
+        })
         if (!job) {
-        throw new customError('There are no job by this id', 404)
-
+            throw new customError('There are no job by this id', 404)
         }
         res.json(job)
     } catch (error) {
-        throw new customError('there are error in job id like it is not completed',404)
+        throw new customError(
+            'there are error in job id like id is not correct',
+            404
+        )
     }
 }
 const creatJob = async (req, res, next) => {
-    // validate req.body 
+    // validate req.body
     // create this job
     // put created id from req.user.userId
-    const createdBy=req.user.userId
-    req.body.createdBy=createdBy
-    const {company,position,status}=req.body
-    if(!company||!position){
+    const createdBy = req.user.userId
+    req.body.createdBy = createdBy
+    const { company, position, status } = req.body
+    if (!company || !position) {
         throw new customError(
             'you should Enter the company name ,and the position',
             404
         )
     }
-    if(status){
-        const AvilableStatus=['interview', 'declined', 'pending']
-        const CorrectStatus=AvilableStatus.includes(status)
-        if(!CorrectStatus){
-         throw new customError(
-             'status for this position is not true you should choice from interview, declined,pending',
-             404
-         )   
+    if (status) {
+        const AvilableStatus = ['interview', 'declined', 'pending']
+        const CorrectStatus = AvilableStatus.includes(status)
+        if (!CorrectStatus) {
+            throw new customError(
+                'status for this position is not true you should choice from interview, declined,pending',
+                404
+            )
         }
     }
-    
-        const job= await Job.create(req.body)
-        res.json(job)
-    }
-    
+
+    const job = await Job.create(req.body)
+    res.json(job)
+}
+
 const updateJob = async (req, res, next) => {
     const jobId = req.params.id
     // var jobId = mongoose.Types.ObjectId(req.params.id)
