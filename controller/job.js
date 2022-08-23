6c19eb67ecd192942bb1afc4ec7fc610900dec15
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-08-23 21:10:44 +0200

    create job

--- controller/job.js
+++ controller/job.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const job = require('../models/job')
 const Job = require('../models/job')
 const customError = require('../error/customError')
+require('express-async-errors')
+
 const getAllJobs = async (req, res, next) => {
     const userId = req.user.userId
     
@@ -17,18 +19,42 @@ const getJob = async (req, res, next) => {
     try {
         const job = await Job.findById(jobId)
         if (!job) {
-            return next('there are no job in this id')
+        throw new customError('There are no job by this id', 404)
+
         }
-        res.send(job)
+        res.json(job)
     } catch (error) {
-        res.json({ error })
+        throw new customError('there are error in job id like it is not completed',404)
     }
 }
 const creatJob = async (req, res, next) => {
-    req.body.createdBy = req.user.userId
-    const job = await Job.create(req.body)
-    res.send(job)
-}
+    // validate req.body 
+    // create this job
+    // put created id from req.user.userId
+    const createdBy=req.user.userId
+    req.body.createdBy=createdBy
+    const {company,position,status}=req.body
+    if(!company||!position){
+        throw new customError(
+            'you should Enter the company name ,and the position',
+            404
+        )
+    }
+    if(status){
+        const AvilableStatus=['interview', 'declined', 'pending']
+        const CorrectStatus=AvilableStatus.includes(status)
+        if(!CorrectStatus){
+         throw new customError(
+             'status for this position is not true you should choice from interview, declined,pending',
+             404
+         )   
+        }
+    }
+    
+        const job= await Job.create(req.body)
+        res.json(job)
+    }
+    
 const updateJob = async (req, res, next) => {
     const jobId = req.params.id
     // var jobId = mongoose.Types.ObjectId(req.params.id)
